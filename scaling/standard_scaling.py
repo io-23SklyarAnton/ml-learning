Fixed scaling. It now calculates mean and std only from the training data

scaling/standard_scaling.py
@@ -1,14 +1,33 @@
-from typing import Optional
-
 import numpy as np
 
 
-def standard_scale(
-        X: np.ndarray
-) -> np.ndarray:
-    X_float = X.astype(float)
+class StandardScaler:
+    def __init__(self):
+        self.mean_ = None
+        self.std_ = None
 
-    mean = np.mean(X_float, axis=0)
-    std = np.std(X_float, axis=0)
+    def fit(
+            self,
+            X: np.ndarray,
+    ) -> 'StandardScaler':
+        self.mean_ = np.mean(X, axis=0)
+        self.std_ = np.std(X, axis=0)
 
-    return (X_float - mean) / std
+        self.std_[self.std_ == 0] = 1.0
+
+        return self
+
+    def transform(
+            self,
+            X: np.ndarray
+    ) -> np.ndarray:
+        if self.mean_ is None or self.std_ is None:
+            raise Exception("Scaler wasn't fitted yet")
+
+        return (X - self.mean_) / self.std_
+
+    def fit_transform(
+            self,
+            X: np.ndarray,
+    ) -> np.ndarray:
+        return self.fit(X).transform(X)

svm/main.py
@@ -1,7 +1,7 @@
 from pandas import read_csv
 import numpy as np
 
-from scaling.standard_scaling import standard_scale
+from scaling.standard_scaling import StandardScaler
 from svm.model import SVMModel
 
 data = read_csv("../iris.csv")
@@ -17,15 +17,16 @@ virginica_mask = np.all(data != 'virginica', axis=1)
 data_without_virginica = data[virginica_mask]
 np.random.shuffle(data_without_virginica)
 
-X = data_without_virginica[:, :-1]
+X = data_without_virginica[:, :-1].astype(float)
 y = data_without_virginica[:, 4]
 
-scaled_data = standard_scale(X)
-
-training_X = scaled_data[:-5]
+training_X = X[:-5]
 training_y = y[:-5]
-unseen_X = scaled_data[-5:]
+unseen_X = X[-5:]
 unseen_y = y[-5:]
+
+standard_scaler = StandardScaler()
+scaled_training_X = standard_scaler.fit_transform(training_X)
 
 _, indices = np.unique(training_y, return_inverse=True)
 correct_answers = []
@@ -35,12 +36,13 @@ for i in indices:
     correct_answers.append(y)
 
 svm.fit(
-    X=training_X,
+    X=scaled_training_X,
     y=np.array(correct_answers),
 )
 
+scaled_unseen_X = standard_scaler.transform(unseen_X)
 for i in range(len(unseen_y)):
     print(
-        svm.predict(unseen_X[i]),
+        svm.predict(scaled_unseen_X[i]),
         unseen_y[i]
     )