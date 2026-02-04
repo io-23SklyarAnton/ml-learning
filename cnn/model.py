dafe1dd91bbb872059bed61702d1cf6354dcbfe7
Anton <80kap.i.toshka@gmail.com>
2026-02-04 15:59:00 +0100

    Added Dense, Flatten and Softmax layers

--- cnn/model.py
+++ cnn/model.py
@@ -169,5 +169,104 @@ class MaxPooling(Layer):
         return d_x
 
 
-class FullyConnectedLayer(Layer):
-    pass
+class Softmax(Layer):
+    def __init__(self):
+        self.cache = None
+
+    def forward_pass(self, x: np.ndarray[np.float64]) -> np.ndarray[np.float64]:
+        shift_x = x - np.max(x)
+        exps = np.exp(shift_x)
+        out = exps / np.sum(exps)
+
+        self.cache = out
+        return out
+
+    def backward_pass(self, delta: np.ndarray[np.float64]) -> np.ndarray[np.float64]:
+        jacobian_m = np.diag(self.cache)
+        jacobian_m -= np.outer(self.cache, self.cache)
+        d_x = np.dot(jacobian_m, delta)
+
+        return d_x
+
+
+class Flatten(Layer):
+    def __init__(self):
+        self.input_shape = None
+
+    def forward_pass(
+            self,
+            x: np.ndarray[np.float64],
+    ) -> np.ndarray[np.float64]:
+        self.input_shape = x.shape
+
+        return x.ravel()
+
+    def backward_pass(
+            self,
+            delta: np.ndarray[np.float64],
+    ) -> np.ndarray[np.float64]:
+        return delta.reshape(self.input_shape)
+
+
+class Dense(Layer):
+    def __init__(
+            self,
+            optimizer_factory: OptimizerFactory,
+            learning_rate: float,
+            in_features: int,
+            out_features: int
+    ) -> None:
+        self.in_features = in_features
+        self.out_features = out_features
+        self._learning_rate = learning_rate
+
+        self._initialize_weights()
+        self._initialize_optimizers(optimizer_factory)
+
+        self._x = None
+
+    def _initialize_weights(self) -> None:
+        scale = np.sqrt(2.0 / self.in_features)
+
+        self._weights = np.random.normal(
+            loc=0.0,
+            scale=scale,
+            size=(self.out_features, self.in_features)
+        )
+
+        self._bias = np.zeros((self.out_features, 1))
+
+    def _initialize_optimizers(
+            self,
+            optimizer_factory: OptimizerFactory
+    ) -> None:
+        self._weight_optimizer = optimizer_factory.create_optimizer()
+        self._bias_optimizer = optimizer_factory.create_optimizer()
+
+    def forward_pass(
+            self,
+            x: np.ndarray[np.float64]
+    ) -> np.ndarray[np.float64]:
+        self._x = x.reshape(-1, 1)
+        z = (self._weights @ self._x) + self._bias
+
+        return z.ravel()
+
+    def backward_pass(
+            self,
+            delta: np.ndarray[np.float64]
+    ) -> np.ndarray[np.float64]:
+        delta = delta.reshape(-1, 1)
+
+        d_w = delta @ self._x.T
+        d_b = delta
+
+        d_x = self._weights.T @ delta
+
+        step_w = self._weight_optimizer.compute_step(d_w)
+        step_b = self._bias_optimizer.compute_step(d_b)
+
+        self._weights -= self._learning_rate * step_w
+        self._bias -= self._learning_rate * step_b
+
+        return d_x.ravel()
