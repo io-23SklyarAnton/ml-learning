1f6f2c465a5aff5b914b1b21a3da700e41b56f79
Anton <80kap.i.toshka@gmail.com>
2026-02-25 01:12:06 +0100

    Optimized data loader, transferred tensors to the right device. Added more logging

--- py_torch_implementation/rnn/data_loader.py
+++ py_torch_implementation/rnn/data_loader.py
@@ -1,31 +1,39 @@
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
+from functools import partial
 
 from py_torch_implementation.rnn.utils import get_token_labels
 
 
+def collate_batch(batch, token_labels):
+    label_list, text_list, lengths = [], [], []
+    for _text, _label in batch:
+        label_list.append(_label)
+
+        processed_text = torch.tensor(
+            data=get_token_labels(_text, token_labels),
+            dtype=torch.int64
+        )
+        text_list.append(processed_text)
+        lengths.append(processed_text.size(dim=0))
+
+    label_list = torch.tensor(label_list, dtype=torch.float32).reshape(-1, 1)
+    lengths = torch.tensor(lengths)
+    padded_text_list = nn.utils.rnn.pad_sequence(text_list, batch_first=True)
+
+    return padded_text_list, label_list, lengths
+
+
 def get_data_loader(dataset, token_labels, batch_size, shuffle) -> DataLoader:
-    def collate_batch(batch):
-        label_list, text_list, lengths = [], [], []
-        for _text, _label in batch:
-            label_list.append(_label)
-            processed_text = torch.tensor(
-                data=get_token_labels(_text, token_labels),
-                dtype=torch.int64
-            )
-            text_list.append(processed_text)
-            lengths.append(processed_text.size(dim=0))
-
-        label_list = torch.tensor(label_list).reshape(-1, 1)
-        lengths = torch.tensor(lengths)
-        padded_text_list = nn.utils.rnn.pad_sequence(text_list, batch_first=True)
-
-        return padded_text_list, label_list, lengths
+    bound_collate_fn = partial(collate_batch, token_labels=token_labels)
 
     return DataLoader(
         dataset=dataset,
         batch_size=batch_size,
         shuffle=shuffle,
-        collate_fn=collate_batch,
+        collate_fn=bound_collate_fn,
+        num_workers=4,
+        pin_memory=True,
+        drop_last=False,
     )
--- py_torch_implementation/rnn/main.py
+++ py_torch_implementation/rnn/main.py
@@ -11,95 +11,110 @@ from py_torch_implementation.rnn.utils import get_token_label_matches
 
 from py_torch_implementation.rnn.lstm_model import Model as LstmModel
 
-df = pd.read_csv("../../datasets/IMDB Dataset.csv")
+if __name__ == '__main__':
+    device = torch.device("mps")
 
-df = df.dropna().reset_index(drop=True)
+    df = pd.read_csv("../../datasets/IMDB Dataset.csv")
 
-df_copy = df.copy()
-df_copy['sentiment'] = df_copy['sentiment'].replace({"positive": 1., "negative": 0.})
+    df = df.dropna().reset_index(drop=True)
 
-train_dataset, test_dataset = train_test_split(df_copy.values, test_size=0.1)
+    df_copy = df.copy()
+    df_copy['sentiment'] = df_copy['sentiment'].replace({"positive": 1., "negative": 0.})
 
-train_dataset, valid_dataset = random_split(train_dataset, [40_000, 5000])
+    train_dataset, test_dataset = train_test_split(df_copy.values, test_size=0.1)
 
-token_labels = get_token_label_matches(train_dataset)
+    train_dataset, valid_dataset = random_split(train_dataset, [40_000, 5000])
 
-train_data_loader = get_data_loader(
-    dataset=train_dataset,
-    batch_size=30,
-    shuffle=True,
-    token_labels=token_labels,
-)
+    token_labels = get_token_label_matches(train_dataset)
 
-test_data_loader = get_data_loader(
-    dataset=test_dataset,
-    batch_size=1,
-    shuffle=False,
-    token_labels=token_labels
-)
+    train_data_loader = get_data_loader(
+        dataset=train_dataset,
+        batch_size=30,
+        shuffle=True,
+        token_labels=token_labels,
+    )
 
-valid_data_loader = get_data_loader(
-    dataset=valid_dataset,
-    batch_size=100,
-    shuffle=False,
-    token_labels=token_labels,
-)
+    test_data_loader = get_data_loader(
+        dataset=test_dataset,
+        batch_size=1,
+        shuffle=False,
+        token_labels=token_labels
+    )
 
-model = LstmModel(
-    num_embeddings=len(token_labels),
-    embedding_dim=100,
-    hidden_size=100
-)
+    valid_data_loader = get_data_loader(
+        dataset=valid_dataset,
+        batch_size=100,
+        shuffle=False,
+        token_labels=token_labels,
+    )
 
-loss_f = BCEWithLogitsLoss()
-optimizer = Adam(model.parameters())
+    model = LstmModel(
+        num_embeddings=len(token_labels),
+        embedding_dim=50,
+        hidden_size=100
+    ).to(device)
 
-for epoch in range(5):
-    correct_guesses = 0
-    total_loss = 0.0
-    total_samples = 0
+    loss_f = BCEWithLogitsLoss()
+    optimizer = Adam(model.parameters())
 
-    model.train()
-    for padded_text_list, label_list, lengths in train_data_loader:
-        optimizer.zero_grad()
-        predict = model(padded_text_list, lengths)
-        loss = loss_f(predict, label_list)
-        loss.backward()
-        optimizer.step()
+    print("LEARNING")
+    for epoch in range(5):
+        correct_guesses = 0
+        total_loss = 0.0
+        total_samples = 0
 
-        total_loss += loss.item()
-        exact_predict = (predict > 0.0).float()
-        correct_guesses += (exact_predict == label_list).sum().item()
-        total_samples += label_list.size(0)
+        model.train()
+        batches_processed = 0
+        for padded_text_list, label_list, lengths in train_data_loader:
+            padded_text_list = padded_text_list.to(device)
+            label_list = label_list.to(device)
+            optimizer.zero_grad()
+            predict = model(padded_text_list, lengths)
+            loss = loss_f(predict, label_list)
+            loss.backward()
+            optimizer.step()
 
-    avg_epoch_loss = total_loss / len(train_data_loader)
-    epoch_accuracy = correct_guesses / total_samples
-    print(f"epoch {epoch} finished\ntrain loss {avg_epoch_loss}\nepoch_accuracy {epoch_accuracy}\n")
+            batches_processed += 1
+            total_loss += loss.item()
+            exact_predict = (predict > 0.0).float()
+            correct_guesses += (exact_predict == label_list).sum().item()
+            total_samples += label_list.size(0)
 
-    model.eval()
+            if batches_processed % 100 == 0:
+                print(f"processed {batches_processed} batches")
+
+        avg_epoch_loss = total_loss / len(train_data_loader)
+        epoch_accuracy = correct_guesses / total_samples
+        print(f"epoch {epoch} finished\ntrain loss {avg_epoch_loss}\nepoch_accuracy {epoch_accuracy}\n")
+
+        model.eval()
+        correct_guesses = 0
+        total_samples = 0
+        with torch.no_grad():
+            for padded_text_list, label_list, lengths in valid_data_loader:
+                padded_text_list = padded_text_list.to(device)
+                label_list = label_list.to(device)
+                predict = model(padded_text_list, lengths)
+
+                exact_predict = (predict > 0.0).float()
+                correct_guesses += (exact_predict == label_list).sum().item()
+                total_samples += label_list.size(0)
+
+            epoch_accuracy = correct_guesses / total_samples
+            print(f"Validation epoch_accuracy: {epoch_accuracy}\n")
+
     correct_guesses = 0
     total_samples = 0
     with torch.no_grad():
-        for padded_text_list, label_list, lengths in valid_data_loader:
+        for padded_text_list, label_list, lengths in test_data_loader:
+            padded_text_list = padded_text_list.to(device)
+            label_list = label_list.to(device)
             predict = model(padded_text_list, lengths)
+            print(f"predict is {sigmoid(predict[0])}, label is {label_list[0]}")
 
             exact_predict = (predict > 0.0).float()
             correct_guesses += (exact_predict == label_list).sum().item()
             total_samples += label_list.size(0)
 
         epoch_accuracy = correct_guesses / total_samples
-        print(f"Validation epoch_accuracy: {epoch_accuracy}\n")
-
-correct_guesses = 0
-total_samples = 0
-with torch.no_grad():
-    for padded_text_list, label_list, lengths in test_data_loader:
-        predict = model(padded_text_list, lengths)
-        print(f"predict is {sigmoid(predict[0])}, label is {label_list[0]}")
-
-        exact_predict = (predict > 0.0).float()
-        correct_guesses += (exact_predict == label_list).sum().item()
-        total_samples += label_list.size(0)
-
-    epoch_accuracy = correct_guesses / total_samples
-    print(f"Test accuracy: {epoch_accuracy}\n")
+        print(f"Test accuracy: {epoch_accuracy}\n")
